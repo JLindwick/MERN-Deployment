6759a6dde669f4296a44a7de7042a1ff0a958570
JLindwick <justin.christopher.russell@gmail.com>
2022-11-08 13:40:53 -0800

    db

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const app = express();
 require('./config/mongoose.config'); 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req,res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 require('./routes/user.routes')(app);
